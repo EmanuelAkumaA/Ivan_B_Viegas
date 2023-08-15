a1d9bc0ca2bddaa80a03f42065285183fb240be2
Emanuel_AkumaA <emmanuel.akuma.dev@gmail.com_AkumaA>
2023-08-14 22:12:54 -0300

    HTML e CSS pronto falta correção dos bugs no JS

---
+++ assets/js/style-switcher.js
@@ -0,0 +1,16 @@
+/* ============================= TOGGLER STYLE SWITCHER ========================== */ 
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+
+// hide style
+window.addEventListener("scroll", () => {
+    if(document.querySelector(".style-switcher").classList.contains("open"))
+    {
+        document.querySelector(".style-switcher").classList.remove("open")
+    }
+})
+
+
+//
